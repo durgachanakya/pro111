05f275382a5660ff11ac1907a554339545e92256
durgachanakya <81852237+durgachanakya@users.noreply.github.com>
2022-04-22 20:20:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,60 @@
-prediction_1 = "";
-prediction_2 = "";
+gesture_1 = "";
+gesture_2 = "";
 
 Webcam.set({
-    width : 350,
-    height : 300,
-    image_format : 'png',
-    png_quality : 90
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
 });
 
 camera = document.getElementById("camera");
-Webcam.attach( '#camera' );
+Webcam.attach('#camera');
 
-function take_snapshot(){
-    Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id= "captured_img" src="'+data_uri+'"/>';
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id= "captured_img" src="' + data_uri + '"/>';
     });
 }
 
-console.log("ml5 version: ",ml5.version);
+console.log("ml5 version: ", ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/V4qKTOZN2/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JwAGDkPac/model.json', modelLoaded);
 
-function modelLoaded(){
+function modelLoaded() {
     console.log('model Loaded!');
 }
 
-function speak(){
+function check() {
+    img = document.getElementById("captured_img");
+    classifier.classify(img, gotresults);
+}
+
+function gotresults(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        gesture = results[0].label;
+        tospeak = "";
+        if (gesture == "best") {
+            tospeak = "all the best";
+            document.getElementById("update_gesture").innerHTML = "&#128077;";
+        } else if (gesture == "amazing") {
+            tospeak = "this looks amazing";
+            document.getElementById("update_gesture").innerHTML = "&#128076;";
+        } else if (gesture == "victory") {
+            tospeak = "wonderful victory";
+            document.getElementById("update_gesture").innerHTML = "&#9996;";
+        }
+        speak()
+    }
+}
+
+function speak() {
     var synth = window.speechSynthesis;
-    speak_data_1 = "The first prediction is " + prediction_1;
-    speak_data_2 = "and the second prediction is " + prediction_2;
-    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    speak_data = tospeak;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
 }
